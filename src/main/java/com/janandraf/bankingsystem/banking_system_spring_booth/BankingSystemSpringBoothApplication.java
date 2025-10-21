fd85c38844adcb338c2ac2b80af0b3810c6aca30
Jan Ryan Ancheta <jan.ryan.ancheta@proton.me>
2025-10-21 15:46:47 +0800

    first commit

---
+++ src/main/java/com/janandraf/bankingsystem/banking_system_spring_booth/BankingSystemSpringBoothApplication.java
@@ -0,0 +1,13 @@
+package com.janandraf.bankingsystem.banking_system_spring_booth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSystemSpringBoothApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSystemSpringBoothApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janandraf/bankingsystem/banking_system_spring_booth/BankingSystemSpringBoothApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janandraf.bankingsystem.banking_system_spring_booth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemSpringBoothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
